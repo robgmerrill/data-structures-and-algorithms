e138daaafd0b0eaf7855b43bdc6c1476fb23bf02
Rob Merrill <robgmerrill@gmail.com>
2016-04-10 19:59:35 -0700

    DNA alternative solution

--- 13_dna_pairing.js
+++ 13_dna_pairing.js
@@ -22,3 +22,29 @@ console.log(pair('ACTG'));
 console.log(pair('cgcg'));
 console.log(pair('atata'));
 
+// Solution 2
+function pair(str) {
+  // Changes string to uupercase for simple checking
+  str = str.toUpperCase();
+  // Breaks string up into a character array
+  str = str.split('');
+  // Empty array to be added to
+  var placeholderArray = [];
+  // Loop to iterate through character array
+  for(var i = 0; i < str.length; i++) {
+    // Checks current character and if equal it addes to final array - Repeat * 4
+    if (str[i] == "G") {
+      placeholderArray.push(['G', 'C']);
+    } else if (str[i] == 'C') {
+      placeholderArray.push(['C', 'G']);
+    } else if (str[i] == 'A') {
+      placeholderArray.push(['A', 'T']);
+    } else if (str[i] == 'T') {
+      placeholderArray.push(['T', 'A']);
+    }
+  }
+    return placeholderArray;
+  
+}
+
+console.log(pair('actg'));
--- 14_palindrome.js
+++ 14_palindrome.js
@@ -1,8 +1,14 @@
 /* Write a simple function that returns a boolean indicating whether 
 or not a string is a palindrome */
 
+// create function named isPalindrome
 function isPalindrome(str) {
+
+  // take string and remove all non-word characters e.g. "," with replace function
+  // turn the string into lower case assuming that it is case insensitive
   str = str.replace(/\W/g, '').toLowerCase();
+
+  // put in parentheses to get a boolean value. Split it into an array. reverse the array. join into a string.
   return (str == str.split('').reverse().join(''));
 }
 
