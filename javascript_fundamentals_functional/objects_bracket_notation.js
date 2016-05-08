8402cb3f1da3932975b27dc903d9ab01d26f6b04
Rob Merrill <robgmerrill@gmail.com>
2016-05-08 11:36:42 -0700

    object bracket notation

--- javascript_fundamentals_functional/objects_bracket_notation.js
+++ javascript_fundamentals_functional/objects_bracket_notation.js
@@ -1,5 +1,12 @@
 var box = {};
 
 box["material"] =  "cardboard";
-
 console.log(box.material) // "cardboard"
+
+var key = "material";
+// key below is not in quotes because it is a variable containing a string and not a string itself
+box[key];
+
+console.log(box[key]); // "cardboard"
+// var key is "material" so we have box["material"] which is "cardboard"
+
